5c3fc114d8d08985ad7a2114709ac2be1c197281
oscariquelme01 <oscariquelme01@gmail.com>
2022-04-17 12:54:25 +0200

    finished mobile navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#toggle-menu').click(function(){
         $(this).toggleClass('toggle-menu-active') 
+        $('.mobile-menu').toggleClass('slide-menu')
     });
 });

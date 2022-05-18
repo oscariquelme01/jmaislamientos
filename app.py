c08aea97d636054bea1f688c17f1c19701bf6fb1
oscariquelme01 <oscariquelme01@gmail.com>
2022-05-18 14:13:26 +0200

    added routes and template files for the different services

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('main.html')
+
+@app.route("/techos-registrables")
+def techos():
+    return render_template('techos.html')
+
+@app.route("/aislamientos")
+def aislamientos():
+    return render_template('aislamientos.html')
+
+@app.route("/pladur")
+def pladur():
+    return render_template('pladur.html')

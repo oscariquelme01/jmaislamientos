f95812e4e28feb3467dd1ef94b4d59428cef5b0d
oscar <oriquelme@rfranco.com>
2024-02-07 08:08:44 +0100

    wip: hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import Image from 'next/image';
 
 function Hero() {
   return (
-    <div className="w-screen relative">
-      <Image
-        src="/images/workers.png"
-        alt="Workers"
-        layout="responsive"
-        width={1920}
-        height={1080}
-        className="object-cover"
-      />
-      <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold text-center">
-        <span className="font-outline-2 text-transparent">JM</span> AISLAMIENTOS Y REVESTIMIENTOS
-      </h1>
+    <div className='w-full h-auto relative overflow-hidden bg-black'>
+      <img src='/images/workers.png' className='w-full opacity-70' alt=""/>
+      <div className='font-extrabold tracking-thin text-center
+        text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl
+        w-4/5 lg:w-[70%]
+        flex flex-col gap-4 md:gap-8 justify-center items-center
+        absolute left-1/2 top-1/2 m-auto -translate-x-[50%] -translate-y-[50%] z-2 text-white'>
+        <div>
+          <span className='text-transparent font-outline-2 xl:font-outline-3
+          text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl
+            relative top-[0.5rem]
+            '>JM </span>
+            Aislamientos<br/> y Revesitimientos
+        </div>
+        <div className='font-normal text-base sm:text-lg md:text-xl xl:text-2xl
+        w-full lg:w-[70%]
+          '>
+          Ayudamos a todo tipo de empresas con soluciones de pladur, insonorizaciones,  revestimientos, aislamientos y más
+        </div>
+      </div>
     </div>
   );
 }

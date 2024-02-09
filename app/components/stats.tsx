58222d53b1dd294d4162f0593b77c5d7f7fb0bcb
oscar <oriquelme@rfranco.com>
2024-02-09 13:09:23 +0100

    feat: scale down mobile stats section

--- app/components/stats.tsx
+++ app/components/stats.tsx
@@ -5,8 +5,8 @@ function Stats() {
   return (
     <div className='my-8 flex flex-col items-center'>
       <div className='box-shadow my-8 flex w-4/5 flex-col items-center justify-center rounded-3xl bg-gradient-accent md:w-[94%] md:flex-row lg:w-4/5'>
-        <div className='grid w-full grid-cols-1 md:grid-cols-[1fr_1fr_1fr] gap-4'>
-          <div className='mt-4 flex items-center justify-center row-start-1 md:row-start-auto'>
+        <div className='grid w-full grid-cols-1 gap-4 md:grid-cols-[1fr_1fr_1fr]'>
+          <div className='row-start-1 mt-4 flex items-center justify-center md:row-start-auto'>
             <Image
               width={0}
               height={0}
@@ -15,7 +15,7 @@ function Stats() {
               className='h-20 w-20 lg:h-28 lg:w-28'
             />
           </div>
-          <div className='mt-4 flex items-center justify-center row-start-4 md:row-start-auto'>
+          <div className='row-start-4 flex items-center justify-center md:row-start-auto md:mt-4'>
             <Image
               width={0}
               height={0}
@@ -24,7 +24,7 @@ function Stats() {
               className='h-20 w-20 lg:h-28 lg:w-28'
             />
           </div>
-          <div className='mt-4 flex items-center justify-center row-start-7 md:row-start-auto'>
+          <div className='row-start-7 flex items-center justify-center md:row-start-auto md:mt-4'>
             <Image
               width={0}
               height={0}
@@ -33,35 +33,35 @@ function Stats() {
               className='h-20 w-20 lg:h-28 lg:w-28'
             />
           </div>
-          <div className='flex items-center justify-center row-start-2 md:row-start-auto'>
-            <div className='text-shadow text-6xl font-bold text-accentLight'>
+          <div className='row-start-2 flex items-center justify-center md:row-start-auto'>
+            <div className='text-shadow text-4xl text-6xl font-bold text-accentLight'>
               +25
             </div>
           </div>
-          <div className='flex items-center justify-center row-start-5 md:row-start-auto'>
-            <div className='text-shadow text-6xl font-bold text-accentLight'>
+          <div className='row-start-5 flex items-center justify-center md:row-start-auto'>
+            <div className='text-shadow text-4xl font-bold text-accentLight lg:text-6xl'>
               +200
             </div>
           </div>
-          <div className='flex items-center justify-center row-start-8 md:row-start-auto'>
-            <div className='text-shadow text-6xl font-bold text-accentLight'>
+          <div className='row-start-8 flex items-center justify-center md:row-start-auto'>
+            <div className='text-shadow text-4xl text-6xl font-bold text-accentLight'>
               +20
             </div>
           </div>
-          <div className='flex items-center justify-center row-start-3 md:row-start-auto'>
-            <div className='box-shadow mb-8 w-3/5 md:w-4/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark'>
+          <div className='row-start-3 flex h-full items-center justify-center md:row-start-auto'>
+            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
               Contamos con más de 25 años de experiencia en el sector
             </div>
           </div>
-          <div className='flex items-center justify-center row-start-6 md:row-start-auto'>
-            <div className='box-shadow mb-8 w-3/5 md:w-4/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark'>
+          <div className='row-start-6 flex h-full items-center justify-center md:row-start-auto'>
+            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
               Hemos completado más de 200 proyectos de todas las dimensiones
             </div>
           </div>
-          <div className='flex items-center justify-center row-start-9 md:row-start-auto'>
-            <div className='box-shadow mb-8 w-3/5 md:w-4/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark'>
+          <div className='row-start-9 flex h-full items-center justify-center md:row-start-auto'>
+            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
               Actualmente, tenemos más de 20 profesionales curtidos en cientos
-              dep proyectos
+              de proyectos
             </div>
           </div>
         </div>

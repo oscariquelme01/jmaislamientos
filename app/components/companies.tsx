fa0edadc75bdeb82fa49eb2b6fff3235f7bd91e0
oscar <oriquelme@rfranco.com>
2024-04-28 11:54:40 +0200

    feat: refactor companies section

--- app/components/companies.tsx
+++ app/components/companies.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import { companiesPath } from '../const';
+'use client'
+
+import React, { useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { companiesPath } from '../const'
 
 function companies() {
   return (
-    <div className='w-full flex justify-center'>
-      <div className='max-w-screen-xl w-full mx-4 mt-8 flex flex-col items-center justify-center lg:mt-16'>
-        <h3 className='w-full text-center text-lg font-semibold text-accentDark md:text-xl lg:text-2xl'>
+    <div className='inline-flex w-full flex-nowrap justify-center'>
+      <div className='mx-4 mt-8 flex w-full max-w-screen-xl flex-col items-center justify-center lg:mt-16'>
+        <h3 className='text-accentDark w-full text-center text-lg font-semibold md:text-xl lg:text-2xl mb-8'>
           Unete al grupo de clientes satisfechos con nuestros servicios
         </h3>
-        <div className='mt-4 flex w-full justify-around md:mt-8'>
-          {companiesPath.map((path, index) => (
-            <img
-              src={`/images/${path}`}
-              alt={`company ${index} logo`}
-              className='h-auto w-[75px] sm:w-[120px] md:w-[160px]'
-              key={index}
-            />
-          ))}
+        <div className='inline-flex w-full max-w-screen-xl flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
+          <ul className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8'>
+            {companiesPath.map((path, index) => (
+              <li>
+                <img
+                  src={`/images/${path}`}
+                  alt={`company ${index} logo`}
+                  className='w-40'
+                  key={index}
+                />
+              </li>
+            ))}
+          </ul>
+
+          {/* Duplicate element to ensure smooth transition */}
+          <ul
+            className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8 relative'
+            aria-hidden={true}
+          >
+            {companiesPath.map((path, index) => (
+              <li>
+                <img
+                  src={`/images/${path}`}
+                  alt={`company ${index} logo`}
+                  className='w-40'
+                  key={index}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default companies;
+export default companies
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'infinite-scroll-x': {
+          from: { transform: 'translateX(0)'},
+          to: { transform: 'translateX(-100%)'}
+        }
+      },
+      animation: {
+        'infinite-scroll-x': 'infinite-scroll-x 25s linear infinite'
+      },
       backgroundImage: {
         'gradient-accent':
           'linear-gradient(0deg, rgba(12,66,117,1) 0%, rgba(12,66,117,0.8) 100%)',

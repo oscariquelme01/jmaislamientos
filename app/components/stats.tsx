58c8a81daccb5ad79a5f19a34788cd4ff34a4d33
oscar <oriquelme@rfranco.com>
2024-04-28 10:53:27 +0200

    feat: refactor stats

--- app/components/stats.tsx
+++ app/components/stats.tsx
@@ -1,73 +1,51 @@
-import React from 'react';
-import Image from 'next/image';
+'use client'
+
+import React, { DOMElement, useEffect, useState, useRef } from 'react'
+import Image from 'next/image'
+import Card from './card'
+import { AcademicCapIcon } from '@heroicons/react/24/outline'
 
 function Stats() {
+  const wrapperRef = useRef(null)
+  const [ isVisible, setIsVisible ] = useState(false)
+
+  const statsData = [
+    {
+      title: 'Experencia',
+      description: 'Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños',
+      icon: AcademicCapIcon
+    },
+    {
+      title: 'Profesionales',
+      description: 'Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños',
+      icon: AcademicCapIcon
+    },
+    {
+      title: 'Proyectos',
+      description: 'Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños',
+      icon: AcademicCapIcon
+    }
+  ]
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries
+    setIsVisible(entry.isIntersecting)
+  }
+
+  useEffect(() => {
+      const observer = new IntersectionObserver(callbackFunction, { root: null, rootMargin: "0px", threshold: 0.85 })
+  })
+
   return (
-    <div className='my-8 flex flex-col items-center'>
-      <div className='max-w-screen-xl box-shadow my-8 flex w-4/5 flex-col items-center justify-center rounded-3xl bg-gradient-accent md:w-[94%] md:flex-row lg:w-4/5'>
-        <div className='grid w-full grid-cols-1 gap-4 md:grid-cols-[1fr_1fr_1fr]'>
-          <div className='row-start-1 mt-4 flex items-center justify-center md:row-start-auto'>
-            <Image
-              width={0}
-              height={0}
-              src='/icons/medal-icon.svg'
-              alt='checklist icon'
-              className='h-20 w-20 lg:h-28 lg:w-28'
-            />
-          </div>
-          <div className='row-start-4 flex items-center justify-center md:row-start-auto md:mt-4'>
-            <Image
-              width={0}
-              height={0}
-              src='/icons/checklist-icon.svg'
-              alt='worker icon'
-              className='h-20 w-20 lg:h-28 lg:w-28'
-            />
-          </div>
-          <div className='row-start-7 flex items-center justify-center md:row-start-auto md:mt-4'>
-            <Image
-              width={0}
-              height={0}
-              src='/icons/worker-icon.svg'
-              alt='medal icon'
-              className='h-20 w-20 lg:h-28 lg:w-28'
-            />
-          </div>
-          <div className='row-start-2 flex items-center justify-center md:row-start-auto'>
-            <div className='text-shadow text-4xl font-bold text-accentLight'>
-              +25
-            </div>
-          </div>
-          <div className='row-start-5 flex items-center justify-center md:row-start-auto'>
-            <div className='text-shadow text-4xl font-bold text-accentLight lg:text-6xl'>
-              +200
-            </div>
-          </div>
-          <div className='row-start-8 flex items-center justify-center md:row-start-auto'>
-            <div className='text-shadow text-4xl font-bold text-accentLight'>
-              +20
-            </div>
-          </div>
-          <div className='row-start-3 flex h-full items-center justify-center md:row-start-auto'>
-            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
-              Contamos con más de 25 años de experiencia en el sector
-            </div>
-          </div>
-          <div className='row-start-6 flex h-full items-center justify-center md:row-start-auto'>
-            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
-              Hemos completado más de 200 proyectos de todas las dimensiones
-            </div>
-          </div>
-          <div className='row-start-9 flex h-full items-center justify-center md:row-start-auto'>
-            <div className='box-shadow m-auto mb-8 h-4/5 w-3/5 rounded-lg bg-white p-4 text-center text-xl text-accentDark md:w-4/5'>
-              Actualmente, tenemos más de 20 profesionales curtidos en cientos
-              de proyectos
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='my-8 flex items-center justify-center gap-6' ref={wrapperRef}>
+      { /* @ts-ignore weird type error where I can't pass an icon from heroicons/react as a prop but can pass any other component */ }
+      <Card title='Experiencia' description='Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños' Icon={AcademicCapIcon}/>
+      { /* @ts-ignore weird type error where I can't pass an icon from heroicons/react as a prop but can pass any other component */ }
+      <Card title='Experiencia' description='Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños' Icon={AcademicCapIcon}/>
+      { /* @ts-ignore weird type error where I can't pass an icon from heroicons/react as a prop but can pass any other component */ }
+      <Card title='Experiencia' description='Contamos con mas de 25 años de experiencia en el sector, cumpliendo con proyectos de todos los tamaños' Icon={AcademicCapIcon}/>
     </div>
-  );
+  )
 }
 
-export default Stats;
+export default Stats

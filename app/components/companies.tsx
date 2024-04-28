3e968480748cefb7ff072a276802aa2f21f261c0
oscar <oriquelme@rfranco.com>
2024-04-28 12:04:57 +0200

    feat: make companies section more responsive

--- app/components/companies.tsx
+++ app/components/companies.tsx
@@ -8,17 +8,17 @@ function companies() {
   return (
     <div className='inline-flex w-full flex-nowrap justify-center'>
       <div className='mx-4 mt-8 flex w-full max-w-screen-xl flex-col items-center justify-center lg:mt-16'>
-        <h3 className='text-accentDark w-full text-center text-lg font-semibold md:text-xl lg:text-2xl mb-8'>
+        <h3 className='w-full text-center text-lg font-semibold md:text-xl lg:text-2xl mb-8'>
           Unete al grupo de clientes satisfechos con nuestros servicios
         </h3>
         <div className='inline-flex w-full max-w-screen-xl flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
-          <ul className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8'>
+          <ul className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none lg:[&_li]:mx-8 [&_li]:mx-4'>
             {companiesPath.map((path, index) => (
               <li>
                 <img
                   src={`/images/${path}`}
                   alt={`company ${index} logo`}
-                  className='w-40'
+                  className='lg:w-40 w-24'
                   key={index}
                 />
               </li>
@@ -27,7 +27,7 @@ function companies() {
 
           {/* Duplicate element to ensure smooth transition */}
           <ul
-            className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8 relative'
+            className='flex animate-infinite-scroll-x items-center justify-center md:justify-start [&_img]:max-w-none lg:[&_li]:mx-8 [&_li]:mx-4'
             aria-hidden={true}
           >
             {companiesPath.map((path, index) => (
@@ -35,7 +35,7 @@ function companies() {
                 <img
                   src={`/images/${path}`}
                   alt={`company ${index} logo`}
-                  className='w-40'
+                  className='lg:w-40 w-24'
                   key={index}
                 />
               </li>
